e5ca9f868ea97ebedd4ee4a1e14e6426837e7361
Alex <best1972@list.ru>
2023-03-28 19:44:04 +0300

    recess

--- Tasks.java
+++ Tasks.java
@@ -2,6 +2,7 @@
 // Задача 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), а так же n! 
 // (произведение чисел от 1 до n) Ввод: 5 Треугольное число 1 + 2 + 3 + 4 + 5 = 15
 // n! 1 * 2 * 3 * 4 * 5 = 120
+
 // public class Tasks {
 //     public static void main(String[] args) {
 //         Scanner in = new Scanner(System.in);
@@ -18,3 +19,24 @@
 //     }
 // }
 
+import java.util.ArrayList;
+import java.util.List;
+// Задача 2. Вывести все простые числа от 1 до 1000
+public class Tasks {
+    public static void main(String[] args) {
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            boolean isPrimeNumber = true;
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    isPrimeNumber = false;
+                    break;
+                }
+            }
+            if (isPrimeNumber) {
+                primes.add(i);
+            }
+        }
+        System.out.println("Простые числа: " + primes);
+    }
+}
