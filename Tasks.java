6c58a36ddcffa84702888ee29fb276645f34bbd9
Alex <best1972@list.ru>
2023-03-28 09:39:30 +0300

    made tasks 1

--- Tasks.java
+++ Tasks.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 // Задача 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), а так же n! 
 // (произведение чисел от 1 до n) Ввод: 5 Треугольное число 1 + 2 + 3 + 4 + 5 = 15
 // n! 1 * 2 * 3 * 4 * 5 = 120
 // public class Tasks {
 //     public static void main(String[] args) {
-//         try (Scanner in = new Scanner(System.in)) {
-//             System.out.print("Введите число: ");
-//             int number = in.nextInt();
-//             int sum = 0;
-//             int fact = 1;
-//             for (int i = 1; i <= number; i++) {
-//                 sum += i;
-//                 fact *= i;
-//             }
-//             System.out.printf("Треугольного число = %s, факториал числа = %s", sum, fact);
-//             }
+//         Scanner in = new Scanner(System.in);
+//         System.out.print("Введите число: ");
+//         int number = in.nextInt();
+//         int sum = 0;
+//         int fact = 1;
+//         for (int i = 1; i <= number; i++) {
+//             sum += i;
+//             fact *= i;
+//         }
+//         System.out.printf("Треугольного число = %s, факториал числа = %s", sum, fact);
+//         in.close();
 //     }
 // }
+
